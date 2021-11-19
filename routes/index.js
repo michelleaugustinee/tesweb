7612f4ba7afc06aadb5219d86e19dbd9d5fec62d
Michelle Augustine <michelleaugustinee@gmail.com>
2021-11-19 19:12:01 +0700

    made changes

--- routes/index.js
+++ routes/index.js
@@ -19,12 +19,15 @@ router.get("/productlist", async (req, res) => {
 
 router.get("/addpro", (req, res) => {
   res.render("pages/addpro");
-})
+});
 
 router.get("/dashboard", async (req, res) => {
   var data = await Product.find();
-  res.render("pages/dashboard", { products: data, message: req.flash("message") });
-})
+  res.render("pages/dashboard", {
+    products: data,
+    message: req.flash("message"),
+  });
+});
 
 router.get("/changepass", (req, res) => {
   res.render("pages/changepass");
@@ -122,18 +125,14 @@ router.get("/add-to-wishlist/:id", (req, res) => {
     req.session.wishlist ? req.session.wishlist : {}
   );
 
-  if (req.session.isLoggedIn == true) {
-    Product.findById(productId, function (err, product) {
-      if (err) {
-        return res.redirect("/productlist");
-      }
-      wishlist.add(product, product.id);
-      req.session.wishlist = wishlist;
-      res.redirect("/productlist");
-    });
-  } else {
-    res.redirect("/login");
-  }
+  Product.findById(productId, function (err, product) {
+    if (err) {
+      return res.redirect("/productlist");
+    }
+    wishlist.add(product, product.id);
+    req.session.wishlist = wishlist;
+    res.redirect("/productlist");
+  });
 });
 
 // Menambahkan Produk yang diinginkan ke dalam Cart dari Page Product List
@@ -141,19 +140,15 @@ router.get("/add-to-cart/:id", (req, res, next) => {
   const productId = req.params.id;
   const cart = new Cart(req.session.cart ? req.session.cart : {});
 
-  if (req.session.isLoggedIn == true) {
-    Product.findById(productId, function (err, product) {
-      if (err) {
-        return res.redirect("/productlist");
-      }
-      cart.add(product, product.id);
-      req.session.cart = cart;
-      console.log(req.session.cart);
-      res.redirect("/productlist");
-    });
-  } else {
-    res.redirect("/login");
-  }
+  Product.findById(productId, function (err, product) {
+    if (err) {
+      return res.redirect("/productlist");
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect("/productlist");
+  });
 });
 
 module.exports = router;
